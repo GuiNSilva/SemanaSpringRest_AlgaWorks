e5fb8f40f59809d4e24bb5dee4a76605aa559011
guilherme <guigarks@gmail.com>
2022-09-24 22:30:56 -0300

    Aula 2.5 implementando o CRUD

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,13 +3,23 @@ package com.algaworks.algalog.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")//mapeando o get a nivel de classe
 public class ClienteController {
 	
 	
@@ -17,10 +27,58 @@ public class ClienteController {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente>listar() {
 		return clienteRepository.findAll();
 		
 	}
+	
+	
+	//Metodo de busca por id
+	
+	@GetMapping("/{clienteId}")//caminho para passar na consulta
+	
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) { //personalizar o resultado na consulta
+		return clienteRepository.findById(clienteId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	 //implementação do metodo post
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)//Mudando o retorno 200 para 201 created
+	public Cliente adicionar(@ RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+		
+	}
+	
+	//implementação do metodo Atualizar
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente>atualizar(@PathVariable Long clienteId,
+			@RequestBody Cliente cliente){
+		if(!clienteRepository.existsById(clienteId)) {// verificando se o cliente não existe
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+		return ResponseEntity.ok(cliente);
+		 
+		
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	//metodo para apagar registros pelo id
+	public ResponseEntity<Void> remover (@PathVariable Long clienteId){
+		if(!clienteRepository.existsById(clienteId)) {// verificando se o cliente não existe
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(clienteId);
+		return ResponseEntity.noContent().build();
+		
+		
+	}
+	
 
 }
--- src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
+++ src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.algaworks.algalog.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,11 @@ import com.algaworks.algalog.domain.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	List<Cliente> findByNome(String nome); // metodo que interface fornece para consulta pelo nome.
+										   //Só mudar o nome do metodo do retorno do cliente repository.
+	
+	
+	List<Cliente> findByNomeContaining(String nome);//mesmo metodo, mas utiliza o contain para pesquisar a string.
 
 }
